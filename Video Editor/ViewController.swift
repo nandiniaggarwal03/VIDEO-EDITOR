0ed02111682eff8bf4275c5b9f0f04c3a1bb22b6
Nandini3103 <61120008+Nandini3103@users.noreply.github.com>
2020-03-26 19:10:38 +0530

    Initial Commit

---
+++ Video Editor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Video Editor
+//
+//  Created by Nandini Aggarwal on 26/03/20.
+//  Copyright © 2020 Nandini Aggarwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
